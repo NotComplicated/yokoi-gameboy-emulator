51c16d608925cdbb7bcccb39352eb048d921152f
Andres Dejesus <andresdejesus123@gmail.com>
2026-02-04 20:05:17 -0600

    separate cart-dump and cart-info subcommands

--- src/cart.rs
+++ src/cart.rs
@@ -1,17 +1,19 @@
-use std::{fmt::Display, path::Path};
+const START_VECTOR: usize = 0x100;
 
 pub struct Cart(Vec<u8>);
 
 #[derive(Debug)]
 pub enum Error {
     Io(std::io::Error),
+    Invalid(&'static str),
 }
 
-impl Display for Error {
+impl std::fmt::Display for Error {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "Errored while reading ROM file: ")?;
+        write!(f, "Errored while reading cartridge file: ")?;
         match self {
             Self::Io(err) => err.fmt(f),
+            Self::Invalid(err) => err.fmt(f),
         }
     }
 }
@@ -19,8 +21,13 @@ impl Display for Error {
 impl std::error::Error for Error {}
 
 impl Cart {
-    pub fn read(path: impl AsRef<Path>) -> Result<Self, Error> {
-        std::fs::read(path).map(Self).map_err(Error::Io)
+    pub fn read(path: impl AsRef<std::path::Path>) -> Result<Self, Error> {
+        let cart = std::fs::read(path).map(Self).map_err(Error::Io)?;
+        if cart.0.len() <= START_VECTOR {
+            Err(Error::Invalid("Cartridge is too small"))
+        } else {
+            Ok(cart)
+        }
     }
 
     pub fn data(&self) -> &[u8] {
--- yokoi-tui/src/main.rs
+++ yokoi-tui/src/main.rs
@@ -11,10 +11,12 @@ struct Cli {
 enum Commands {
     /// Print cartridge information
     CartInfo {
-        /// Hex-dump the cartridge file contents
-        #[arg(long)]
-        dump: bool,
+        /// Path to cartridge file
+        path: PathBuf,
+    },
 
+    /// Hex-dump cartridge contents
+    CartDump {
         /// Path to cartridge file
         path: PathBuf,
     },
@@ -30,20 +32,21 @@ fn run() -> Result<(), Box<dyn std::error::Error>> {
     let cli = Cli::parse();
     let mut out = std::io::stdout().lock();
     match cli.command {
-        Commands::CartInfo { dump, path } => {
+        Commands::CartInfo { path } => {
             let cart = yokoi::cart::Cart::read(&path)?;
-            if dump {
-                let width = crossterm::terminal::size()?.0;
-                let chunk_size = ((width as usize - "000000:  ".len()) / 3).next_power_of_two() / 2;
-                for (i, chunk) in cart.data().chunks(chunk_size).enumerate() {
-                    write!(&mut out, "{:06X}:  ", i * chunk_size)?;
-                    for byte in chunk {
-                        write!(&mut out, "{byte:02X} ")?;
-                    }
-                    writeln!(&mut out)?;
+            write!(out, "Bytes: {}", cart.data().len())?;
+        }
+        Commands::CartDump { path } => {
+            let cart = yokoi::cart::Cart::read(&path)?;
+            let width = crossterm::terminal::size()?.0;
+            let chunk_size = ((width as usize - "000000:".len()) / 3).next_power_of_two() / 2;
+            for (i, chunk) in cart.data().chunks(chunk_size).enumerate() {
+                write!(out, "{:06X}:", i * chunk_size)?;
+                for byte in chunk {
+                    write!(out, " {byte:02X}")?;
                 }
+                writeln!(out)?;
             }
-            println!("{}", cart.data().len());
         }
     }
     Ok(())
