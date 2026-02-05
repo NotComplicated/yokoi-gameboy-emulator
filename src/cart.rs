bfc0a1fd5ea776d22752f88957b8f8bc659eb967
Andres Dejesus <andresdejesus123@gmail.com>
2026-02-04 20:20:47 -0600

    thiserror

--- src/cart.rs
+++ src/cart.rs
@@ -2,31 +2,21 @@ const START_VECTOR: usize = 0x100;
 
 pub struct Cart(Vec<u8>);
 
-#[derive(Debug)]
+#[derive(thiserror::Error, Debug)]
 pub enum Error {
-    Io(std::io::Error),
+    #[error("Errored while reading cartridge file: {0}")]
+    Io(#[from] std::io::Error),
+    #[error("Invalid cartridge file: {0}")]
     Invalid(&'static str),
 }
 
-impl std::fmt::Display for Error {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "Errored while reading cartridge file: ")?;
-        match self {
-            Self::Io(err) => err.fmt(f),
-            Self::Invalid(err) => err.fmt(f),
-        }
-    }
-}
-
-impl std::error::Error for Error {}
-
 impl Cart {
     pub fn read(path: impl AsRef<std::path::Path>) -> Result<Self, Error> {
-        let cart = std::fs::read(path).map(Self).map_err(Error::Io)?;
-        if cart.0.len() <= START_VECTOR {
-            Err(Error::Invalid("Cartridge is too small"))
+        let data = std::fs::read(path)?;
+        if data.len() <= START_VECTOR {
+            Err(Error::Invalid("not enough data"))
         } else {
-            Ok(cart)
+            Ok(Self(data))
         }
     }
 
