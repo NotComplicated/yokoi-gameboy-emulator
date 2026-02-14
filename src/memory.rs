48fd9a50cd16c59ddd452f0db7a7ec0aa23fbbc9
Andres Dejesus <andresdejesus123@gmail.com>
2026-02-14 02:10:13 -0600

    a few changes to memory

--- src/memory.rs
+++ src/memory.rs
@@ -9,11 +9,12 @@ pub struct Memory {
     mode: Mode,
     boot_rom: Vec<u8>,
     cart: Cart,
-    rom: Rom,
+    rom_bank: u8,
     vram: [u8; 8 * 1024],
     sram: [u8; 8 * 1024],
     wram0: [u8; 4 * 1024],
     wramx: [[u8; 4 * 1024]; 8],
+    wramx_bank: u8,
     oam: [u8; 160],
     hram: [u8; 256],
 }
@@ -24,6 +25,8 @@ pub struct Rom;
 #[derive(Debug)]
 pub enum Error {
     Op(opcode::Error),
+    LenMismatch,
+    OutOfBounds,
 }
 
 impl Memory {
@@ -32,25 +35,44 @@ impl Memory {
             mode,
             boot_rom,
             cart,
-            rom: Rom,
+            rom_bank: 1,
             vram: [0; _],
             sram: [0; _],
             wram0: [0; _],
             wramx: [[0; _]; _],
+            wramx_bank: 1,
             oam: [0; _],
             hram: [0; _],
         }
     }
 
     pub fn read(&self, addr: u16) -> Result<u8, Error> {
-        Ok(0)
+        self.read_inner(addr, 1).map(|mem| mem[0])
     }
 
     pub fn read_op(&self, pc: u16) -> Result<(Op, u16), Error> {
-        Ok((Op::Nop, pc))
+        let mem = self.read_inner(pc, 1)?;
+        Op::read(mem)
+            .map(|(op, new_mem)| (op, pc + (new_mem.len() - mem.len()) as u16))
+            .map_err(Error::Op)
+    }
+
+    fn read_inner(&self, addr: u16, len: u16) -> Result<&[u8], Error> {
+        match addr {
+            _ => Err(Error::OutOfBounds),
+        }
     }
 
     pub fn write(&mut self, addr: u16, data: u8) -> Result<(), Error> {
-        Ok(())
+        self.write_inner(addr, 1, &[data])
+    }
+
+    pub fn write_inner(&mut self, addr: u16, len: u16, data: &[u8]) -> Result<(), Error> {
+        if data.len() != len.into() {
+            return Err(Error::LenMismatch);
+        }
+        match addr {
+            _ => Err(Error::OutOfBounds),
+        }
     }
 }
